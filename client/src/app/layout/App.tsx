c356149b4d5efa38acb819e4149fd723101ab1eb
neha <cneha7250@gmail.com>
2023-02-23 16:40:18 +0530

    Second Commit

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,35 @@
+import { Container, createTheme, CssBaseline, ThemeProvider, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import Catalog from "../../featues/catalog/Catalog";
+import { Product } from "../models/Product";
+import Header from "./Header";
+
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const palleteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: palleteType,
+      background: {
+        default: (palleteType === 'light') ? '#eaeaea' : '#121212'
+      }
+    }
+  })
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+  
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}  />
+      <Container>
+        <Catalog />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
